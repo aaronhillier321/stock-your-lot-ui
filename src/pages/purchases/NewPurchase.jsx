ffddfdac3885cd8c54a4920cda13b3b4b49eb615
Aaron Hillier <aaronhillier321@gmail.com>
2026-02-14 08:43:51 -0800

    dockerfile

--- src/pages/purchases/NewPurchase.jsx
+++ src/pages/purchases/NewPurchase.jsx
@@ -7,6 +7,15 @@ import PdfPreviewModal from '../../components/PdfPreviewModal'
 import carImage from '../../../assets/Car.png'
 import './NewPurchase.css'
 
+function randomUUID() {
+  if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+    return crypto.randomUUID()
+  }
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+  )
+}
+
 const emptyForm = {
   dealershipId: '',
   date: '',
@@ -99,7 +108,7 @@ export default function NewPurchase() {
     setError('')
     setConflictErrors({})
     setExtracting(true)
-    const token = crypto.randomUUID()
+    const token = randomUUID()
     try {
       const billForm = new FormData()
       billForm.append('file', billOfSaleFile)
